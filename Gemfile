54447dbc9cf58b90626365d799ede3f4156cc833
Shane Mingins <shane@mingins>
2011-08-05 16:15:24 +1200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'sqlite3'
+
+group :development, :test do
+  gem "rspec-rails", ">= 2.6.1"
+  gem "capybara", "~> 1.0.0"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capybara::Application.config.secret_token = '711da2080e746e91e08279b6324e08009fd70b6d8e75498d5559923f14d082a0acf8e55afa5f9e8e615de46185f89e986dedff4c7cb4524fbf763fd6a2352fa9'
